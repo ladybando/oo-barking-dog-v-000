1a0851127b54a1477280faa51fcc11b313f6a339
Ayana Bando <ayanaflax@yahoo.com>
2017-11-09 21:01:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog
+  def dog=(name,bark)
+    @name = name
+    @bark = bark
+  end
+
+ def bark
+  puts "woof!"
+ end
+
+ def name=(new_name)
+  @name = new_name
+ end
+end
